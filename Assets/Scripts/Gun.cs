9e3ae945ac717517cfec69d8a216e22168c8ce4c
AlexFaernon <alexpavlenko6666@rambler.ru>
2022-10-11 16:59:38 +0500

    NiceHook

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,9 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
+    [SerializeField] private GameObject hook;
+    private LineRenderer line;
+    private void Start()
+    {
+        line = GetComponent<LineRenderer>();
+    }
+
     void Update()
     {
         var mousePosition = Input.mousePosition;
@@ -11,5 +19,7 @@ public class Gun : MonoBehaviour
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition); //положение мыши из экранных в мировые координаты
         var angle = Vector2.Angle(Vector2.right, mousePosition - transform.position);//угол между вектором от объекта к мыше и осью х
         transform.eulerAngles = new Vector3(0f, 0f, transform.position.y < mousePosition.y ? angle : -angle);//немного магии на последок
+        line.SetPosition(0, transform.position);
+        line.SetPosition(1, hook.transform.position);
     }
 }
--- Assets/Scripts/Hook.cs
+++ Assets/Scripts/Hook.cs
@@ -5,21 +5,43 @@ using UnityEngine;
 
 public class Hook : MonoBehaviour
 {
-    private float baseX;
-
-    public static bool isLaunched;
-    public static bool isRectracting;
+    private bool isLaunched;
+    private bool isRectracting;
 
     private RectTransform _rectTransform;
+
+    private SpringJoint2D spring;
+
+    private BoxCollider2D boxCollider2D;
+
     // Update is called once per frame
     private void Start()
     {
-        baseX = transform.position.x;
         _rectTransform = GetComponent<RectTransform>();
+        spring = GetComponent<SpringJoint2D>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
     }
 
     void Update()
     {
+        if (isRectracting)
+        {
+            if (_rectTransform.anchoredPosition.x > 110)
+            {
+                transform.Translate(Vector3.up * (Time.deltaTime * 3));
+            }
+            else
+            {
+                isRectracting = false;
+                if (spring.connectedBody == null) return;
+
+                Destroy(spring.connectedBody.gameObject);
+                spring.connectedBody = null;
+                boxCollider2D.enabled = true;
+            }
+            return;
+        }
+        
         if (isLaunched)
         {
             if (_rectTransform.anchoredPosition.x < 300)
@@ -34,21 +56,16 @@ public class Hook : MonoBehaviour
             return;
         }
 
-        if (isRectracting)
-        {
-            if (_rectTransform.anchoredPosition.x > 110)
-            {
-                transform.Translate(Vector3.up * (Time.deltaTime * 3));
-            }
-            else
-            {
-                isRectracting = false;
-            }
-            return;
-        }
-
         if (!Input.GetMouseButtonDown(0)) return;
         Debug.Log("succ");
         isLaunched = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D item)
+    {
+        spring.connectedBody = item.attachedRigidbody;
+        isLaunched = false;
+        isRectracting = true;
+        boxCollider2D.enabled = false;
     }
 }
