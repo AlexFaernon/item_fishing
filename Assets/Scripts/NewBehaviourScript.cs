1dc07538b62285638c1f0f22b8797e732075bed3
AlexFaernon <alexpavlenko6666@rambler.ru>
2022-12-05 17:46:47 +0500

    Research

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Research
+{
+    public static bool TurretsUpgrade;
+    public static bool TwoTurretsUpgrade;
+    public static bool BarriersUpgrade;
+}
