8f300dc01e56018827c56afdb2fc51cf778b1122
sckmat <schmat03@gmail.com>
2023-01-07 23:31:31 +0600

    Yandex Plugin

--- Assets/Scripts/Coins.cs
+++ Assets/Scripts/Coins.cs
@@ -1,28 +1,26 @@
 using System;
 using TMPro;
 using UnityEngine;
+using YG;
 
 public class Coins : MonoBehaviour
 {
     [SerializeField]private TMP_Text coinsCountText;
-    private YandexSDK sdk;
-
-    private void Start()
-    {
-        sdk = YandexSDK.instance;
-        sdk.onRewardedAdReward += Reward;
-    }
-
+    private void OnEnable() => YandexGame.RewardVideoEvent += Rewarded;
+    private void OnDisable() => YandexGame.RewardVideoEvent -= Rewarded;
     private void Update()
     {
         coinsCountText.text = Resources.Coins.ToString();
     }
 
-    public void Reward(string placement)
+    private void Rewarded(int id)
     {
-      if (placement == "coin")
-      {
-          PowerUps.AddToken();
-      }
+        PowerUps.AddToken();
+        Debug.Log("Add Token");
+    }
+
+    public void OpenRewardedAd(int id)
+    {
+        YandexGame.RewVideoShow(id);
     }
 }
